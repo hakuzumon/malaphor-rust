e3a7d9ab979796e86458ba1da37860aa7511837c
Martti Soininen <martti.soininen@evident.fi>
2020-09-07 22:38:19 +0300

    Implement malaphor.generate()

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod malaphor {
     use rand::distributions::Uniform;
     use rand::prelude::*;
 
+    #[derive(PartialEq)]
     #[derive(Debug)]
     struct SentencePart {
         first_word_lowercase: String,
@@ -38,6 +39,7 @@ mod malaphor {
         }
     }
 
+    #[derive(PartialEq)]
     #[derive(Debug)]
     pub struct Sentence {
         parts: Vec<SentencePart>
@@ -106,9 +108,75 @@ mod malaphor {
         }
 
         pub fn random_sentence(&self) -> &Sentence {
+            let choices = &self.data;
             let mut rng = self.rng;
-            let sample_index = rng.sample(Uniform::new(0, self.data.len()));
-            &self.data[sample_index]
+            let sample_index = rng.sample(Uniform::new(0, choices.len()));
+            &choices[sample_index]
+        }
+
+        pub fn random<'a>(&self, choices: &Vec<&'a Sentence>) -> &'a Sentence {
+            let mut rng = self.rng;
+            let sample_index = rng.sample(Uniform::new(0, choices.len()));
+            choices[sample_index]
+        }
+
+        pub fn find_matches(&self, sentence: &Sentence) -> Vec<&Sentence> {
+            let good_matches = self.find_good_matches(sentence);
+            let bad_match_percentage = self.get_bad_match_percentage(good_matches.len());
+            let mut rng = self.rng;
+            let d100 = rng.sample(Uniform::new(0, 100));
+
+            if d100 <= bad_match_percentage {
+                self.find_bad_matches(sentence)
+            } else {
+                good_matches
+            }
+        }
+
+        fn find_good_matches(&self, sentence: &Sentence) -> Vec<&Sentence> {
+            self.data.iter()
+                .filter(|s| s != &sentence &&
+                s.parts[1].first_word_lowercase == sentence.parts[1].first_word_lowercase)
+                .collect()
+        }
+
+        fn find_bad_matches(&self, sentence: &Sentence) -> Vec<&Sentence> {
+            self.data.iter()
+                .filter(|s| s != &sentence)
+                .collect()
+        }
+
+        fn get_bad_match_percentage(&self, option_count: usize) -> u8 {
+            match option_count {
+                1 => 95,
+                2 => 90,
+                3 => 80,
+                4 | 5 | 6 => 70,
+                7 | 8 | 9 => 60,
+                10 | 11 | 12 | 13 | 14 => 30,
+                _ => 15
+            }
+        }
+
+        fn combine_in_any_order(&self, s1: &Sentence, s2: &Sentence) -> String {
+            let mut rng = self.rng;
+            if rng.gen_bool(0.5) {
+                self.combine(s1, s2)
+            } else {
+                self.combine(s2, s1)
+            }
+        }
+
+        fn combine(&self, begin: &Sentence, end: &Sentence) -> String {
+            begin.parts[0].part.to_string() + ", " + end.parts[1].part.to_string().as_str()
+        }
+
+        pub fn generate(&self) -> String {
+            let s1 = self.random_sentence();
+            let matches = self.find_matches(&s1);
+            let s2 = self.random(&matches);
+
+            self.combine_in_any_order(s1, s2)
         }
     }
 }
@@ -116,5 +184,7 @@ mod malaphor {
 fn main() {
     let malaphor = malaphor::Malaphor::init("./src/sentences.txt");
 
-    println!("random: {}", malaphor.random_sentence());
+    let result = malaphor.generate();
+
+    println!("random malaphor: {}", result);
 }
